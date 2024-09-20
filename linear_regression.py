47d058a6eecbdd6077e14a591ef4935a8bc1b852
Andrei Chiorian <achiorian23@yahoo.com>
2024-09-20 13:56:45 +0200

    last

--- linear_regression.py
+++ linear_regression.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from colors import Colors
+
+# Colors for the output
 
 
 # Cost of a security incident
@@ -9,14 +12,22 @@ from sklearn.linear_model import LinearRegression
 # The exercise consists of predicting the cost of a security incident based on the number
 # of computers that have been affected. The data set is generated randomly.
 def linear_regression():
-    x = 2 * np.random.rand(1000, 1)
-    y = 4 + 3 * x + np.random.randn(1000, 1)
+    print(Colors.HEADER, Colors.UNDERLINE, "\nLINEAR REGRESSION ALGORITHM'S \n", Colors.ENDC)
+    print(Colors.HEADER, "\nThe exercise consists of predicting the cost of a security incident based on the number\n"
+                         "of computers that have been affected. In this case the data set is generated randomly in real\n"
+                         "situations should use real data recollected from past experiences. \n\n", Colors.ENDC)
 
-    print("The length of the data set is:", len(x))
+    num_sample = int(input(Colors.OKCYAN + "\nEnter the number of past security incidents the data set should contain? (!max-100000000)\n" + Colors.ENDC))
+
+    x = 2 * np.random.rand(num_sample, 1)
+    y = 4 + 3 * x + np.random.randn(num_sample, 1)
+
+    print(Colors.WARNING, "\nThe length of the data set is:", Colors.ENDC, Colors.OKGREEN, len(x), '\n', Colors.ENDC)
 
     data = {'n_affected_computers': x.flatten(), 'cost': y.flatten()}
     df = pd.DataFrame(data)
 
+    print(Colors.WARNING, "\nGenerated data set" , Colors.ENDC)
     print(df)
 
     # Scaling the number of affected computers
@@ -27,6 +38,7 @@ def linear_regression():
     df['cost'] = df['cost'].astype('int')
     df.head(10)
 
+    print(Colors.WARNING, "\n1000x scaled data set", Colors.ENDC)
     print(df)
 
     # Graphical representation of the data set
@@ -38,13 +50,16 @@ def linear_regression():
 
     # Construction of the model and adjustment of the hypothesis function
     lin_reg = LinearRegression()
+
     lin_reg.fit(df['n_affected_computers'].values.reshape(-1, 1), df['cost'].values)
 
     # Theta parameter 0
-    print(lin_reg.intercept_)
+    print(Colors.WARNING, "\nTheta parameter 0 (starting point of x)", Colors.ENDC)
+    print(Colors.OKGREEN, round(lin_reg.intercept_, 3), Colors.ENDC)
 
     # Theta parameter 1
-    print(lin_reg.coef_)
+    print(Colors.WARNING, "\nTheta parameter 1 (coefficient, slope of the line)", Colors.ENDC)
+    print(Colors.OKGREEN, round(float(lin_reg.coef_), 3), Colors.ENDC)
 
     # Prediction for the minimum and maximum value of the training data set
     x_min_max = np.array([[df["n_affected_computers"].min()], [df["n_affected_computers"].max()]])
@@ -59,12 +74,15 @@ def linear_regression():
     plt.ylabel("Incident cost")
 
     # Predicting new examples
-    x_new = np.array([[1300]])  # 1300 computers affected
+    x_new = np.array([[int(input(Colors.OKCYAN + "\nEnter the number of affected equipment?\n" + Colors.ENDC))]])
 
     # Prediction of the cost that the incident would have
     cost = lin_reg.predict(x_new)
 
-    print("The cost of the incident for", int(x_new[0][0]), "affected equipment would be:", int(cost[0]), "€")
+    print(Colors.OKBLUE, "\nThe cost of the incident for", Colors.OKGREEN, int(x_new[0][0]), Colors.OKBLUE,
+          "affected equipment would be:", Colors.OKGREEN, int(cost[0]), "€\n")
+
+    print(Colors.WARNING, "\nClose graphics tabs to continue", Colors.ENDC,)
 
     plt.figure(figsize=(10, 5))
     plt.title('Graphical representation of the prediction of the cost that the incident would have')
@@ -73,5 +91,4 @@ def linear_regression():
     plt.plot(x_new, cost, "rx")
     plt.xlabel("Affected equipment")
     plt.ylabel("Incident cost")
-
     plt.show()
--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 from linear_regression import linear_regression
+from colors import Colors
 
 
 if __name__ == '__main__':
 
-    linear_regression()
+    while True:
+        print(Colors.HEADER, Colors.UNDERLINE, "\nMACHINE LEARNING ALGORITHM'S \n\n", Colors.ENDC)
+
+        print(Colors.OKBLUE, "- 1 - Linear Regression", Colors.ENDC)
+
+        print(Colors.OKBLUE, "\n - 2 - Logistic regression", Colors.ENDC)
+
+        print(Colors.OKBLUE, "\n - 3 - Linear Regression", Colors.ENDC)
+
+        print(Colors.OKBLUE, "\n - 4 - Linear Regression", Colors.ENDC)
+
+        algorithm = input(Colors.OKCYAN + "\n¿Enter the number of the machine learning algorithm would you like to run?"
+                                          "\n" + Colors.ENDC)
+
+        if algorithm:
+            match int(algorithm):
+                case 1:
+                    linear_regression()
+                    print(Colors.OKCYAN, "\n\nLinear Regression Algorithm example completed", Colors.ENDC)
+                    print(Colors.FAIL, "\n\n-------------------------------------------------------------------------"
+                                         "--------------", Colors.ENDC)
+                case 2:
+                    print(Colors.OKCYAN, "\n\nLogistic Regression Algorithm example completed", Colors.ENDC)
+                case 3:
+                    print(Colors.OKCYAN, "Opción 3 seleccionada", Colors.ENDC)
+                case _:
+                    print(Colors.OKCYAN, "Opción no válida", Colors.ENDC)
+
 
